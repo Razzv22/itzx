8e7ff7c61a40b91d4d6d3d8a83cc6039548a89e4
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-09 13:41:01 +0700

    Update __a.js

--- plugins/__a.js
+++ plugins/__a.js
@@ -1,3 +1,4 @@
+/*
 if video / image = m, false, { blablabla
 conn.sendFile(m.chat, cita, 'asupan.mp4', 'Nih kak asupan nya!!', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
@@ -6,4 +7,4 @@ if button / image with button = m, { blablabla
 m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
 //@${who.split`@`[0]}        /////@${m.sender.split`@`[0]}
-
+*/
