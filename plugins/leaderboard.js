479036baf974a0580b2d888e7fc8e48edb81737c
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-10 23:08:29 +0700

    Update leaderboard.js

--- plugins/leaderboard.js
+++ plugins/leaderboard.js
@@ -2,6 +2,7 @@ let handler = async (m, { conn, args, participants }) => {
   let users = Object.entries(global.db.data.users).map(([key, value]) => {
     return { ...value, jid: key }
   })
+  let name = m.fromMe ? conn.user : conn.contacts[m.sender]
   let sortedExp = users.map(toNumber('exp')).sort(sort('exp'))
   let sortedLim = users.map(toNumber('limit')).sort(sort('limit'))
   let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
@@ -15,21 +16,26 @@ let handler = async (m, { conn, args, participants }) => {
   console.log(participants)
   let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
   let text = `
-┌〔 *XP Leaderboard Top ${len}* 〕
-├ Kamu: *${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
-│
-${sortedExp.slice(0, len).map(({ jid, exp }, i) => `├ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} Exp*`).join`\n`}
-└────
-┌〔 *Limit Leaderboard Top ${len}* 〕
-├ Kamu: *${usersLim.indexOf(m.sender) + 1}* dari *${usersLim.length}*
-│
-${sortedLim.slice(0, len).map(({ jid, limit }, i) => `├ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} Limit*`).join`\n`}
-└────
-┌〔 *Level Leaderboard Top ${len}* 〕
-├ Kamu: *${usersLevel.indexOf(m.sender) + 1}* dari *${usersLevel.length}*
-│
-${sortedLevel.slice(0, len).map(({ jid, level }, i) => `├ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Level ${level}*`).join`\n`}
-└────`.trim()
+• *XP Leaderboard Top ${len}* •
+Kamu: *${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
+
+${sortedExp.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.exp + ' Lvl*').join`\n`}
+
+• *Limit Leaderboard Top ${len}* •
+Kamu: *${usersLim.indexOf(m.sender) + 1}* dari *${usersLim.length}*
+
+${sortedLim.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.limit + ' Lvl*').join`\n`}
+
+• *Level Leaderboard Top ${len}* •
+Kamu: *${usersLevel.indexOf(m.sender) + 1}* dari *${usersLevel.length}*
+
+${sortedLevel.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.level + ' Lvl*').join`\n`}
+
+• *Money Leaderboard Top ${len}* •
+Kamu: *${usersMoney.indexOf(m.sender) + 1}* dari *${usersMoney.length}*
+
+${sortedMoney.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.money + ' Money*').join`\n`}
+`.trim()
   conn.reply(m.chat, text, m, {
     contextInfo: {
       mentionedJid: [...usersExp.slice(0, len), ...usersLim.slice(0, len), ...usersLevel.slice(0, len)].filter(v => !participants.some(p => v === p.jid))
