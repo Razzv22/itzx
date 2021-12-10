247cf088cb6840bc68827c37961d0bbc0c5d3ea3
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-10 10:32:04 +0700

    Update levelup.js

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -1,80 +1,24 @@
 let levelling = require('../lib/levelling')
-const canvacord = require('canvacord')
 
-let handler = async (m, { conn, usedPrefix }) => {
-  let pp = './src/avatar_contact.png'
-  let bekgron = './src/bgCanva.png'
-  let who = m.sender
-  let discriminator = who.substring(9, 13)
-  try {
-    pp = await conn.getProfilePicture(who)
-  } catch (e) {
-  } finally {
-    let { exp, limit, level, role, registered } = global.db.data.users[m.sender]
-    let users = Object.entries(global.db.data.users).map(([key, value]) => {
-      return { ...value, jid: key }
-    })
-    let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
-    let usersLevel = sortedLevel.map(enumGetKey)
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    if (!levelling.canLevelUp(level, exp, global.multiplier)) {
-    	cantUp = `Level *${level} (${exp - min}/${xp})*\nKurang *${max - exp}* lagi!`
-      let rank = await new canvacord.Rank()
-        .setRank(usersLevel.indexOf(m.sender) + 1)
-        .setAvatar(pp)
-        .setLevel(level)
-        .setCurrentXP(exp - min)
-        .setRequiredXP(xp)
-        .setProgressBar("#f2aa4c", "COLOR")
-        .setBackground("IMAGE", bekgron)
-        .setUsername(conn.getName(who))
-        .setDiscriminator(discriminator)
-      rank.build()
-        .then(async (data) => {
-          await conn.sendButtonImg(m.chat, data, cantUp.trim(), '© stikerin', 'Auto Level Up', `${usedPrefix}on autolevelup`, m, { thumbnail: data, height: 282, width: 934 })
-        })
-    }
-    let before = level * 1
-    while (levelling.canLevelUp(level, exp, global.multiplier)) level++
-    if (before !== level) {
-    	canUp = `_*Level Up!*_\n_${before}_ -> _${level}_`.trim(), '© stikerin', 'Auto Level Up', `${usedPrefix}on autolevelup`
-      let rank = await new canvacord.Rank()
-        .setRank(usersLevel.indexOf(m.sender) + 1)
-        .setAvatar(pp)
-        .setLevel(level)
-        .setCurrentXP(exp - min)
-        .setRequiredXP(xp)
-        .setProgressBar("#f2aa4c", "COLOR")
-        .setBackground("IMAGE", bekgron)
-        .setUsername(conn.getName(who))
-        .setDiscriminator(discriminator)
-      rank.build()
-        .then(async (data) => {
-          await conn.sendButtonImg(m.chat, data, canUp.trim(), '© stikerin', 'Auto Level Up', `${usedPrefix}on autolevelup`, m, { thumbnail: data, height: 282, width: 934 })
-        })
-    }
+let handler = m => {
+  let user = global.db.data.users[m.sender]
+  if (!levelling.canLevelUp(user.level, user.exp, global.multiplier)) {
+    let { min, max } = levelling.xpRange(user.level, user.exp, global.multiplier)
+    return m.reply(`
+Level *${user.level} (${user.exp}/${max})*
+Kurang *${min} XP* lagi!
+`.trim())
   }
+  user.level++
+  m.reply(`
+Selamat, anda telah naik level!
+*${user.level - 1}* -> *${user.level}*
+  `.trim())
 }
 
 handler.help = ['levelup']
-handler.tags = ['xp']
+handler.tags = ['rpg']
 
 handler.command = /^levelup$/i
 
 module.exports = handler
-
-function sort(property, ascending = true) {
-  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
-  else return (...args) => args[ascending & 1] - args[!ascending & 1]
-}
-
-function toNumber(property, _default = 0) {
-  if (property) return (a, i, b) => {
-    return { ...b[i], [property]: a[property] === undefined ? _default : a[property] }
-  }
-  else return a => a === undefined ? _default : a
-}
-
-function enumGetKey(a) {
-  return a.jid
-}
