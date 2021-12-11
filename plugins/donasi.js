7ef5b0fb21c4bf753f023308eea9089575531aee
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-11 09:44:20 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-╭─˗ˏˋ*Donasi • Emoney - Pulsa* ´ˎ˗
-│ ✎ PULSA : 081998903280
-│ ✎ Dana/Ovo/Gopay : 081998903280
-│ ✎ Bila Perlu hubungi owner 
-╰‿‿‿‿‿‿‿‿‿‿‿‿‿‿
+*Donasi • Emoney - Pulsa* ´ˎ˗
+✎ PULSA : +62 813-2663-5396
+✎ Dana / ~Ovo~ / Gopay : +62 888-5960-825
+✎ Bila Perlu hubungi owner 
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
