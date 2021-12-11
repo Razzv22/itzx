6dce932c3cf0f2a4ed9a1c5ee0c808fdfb2fb1d0
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-11 19:25:33 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,25 +1,19 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'Uhm..url nya mana?'
-m.reply(wait)
-let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.status) throw json
-let { description, author, statistic, link } = json.result
-await conn.sendFile(m.chat, link, 'tt.mp4', `
-▶ ${statistic.playCount} Views
-❤ ${statistic.diggCount} Likes
-🔁 ${statistic.shareCount} Shares
-💬 ${statistic.commentCount} Comments
-- *By:* ${author.nickname} (${author.username})
-- *Desc:*
-${description}
-`.trim(), m)
-}
 
-handler.help = ['tiktok <url>']
-handler.tags = ['downloader']
-handler.command = /^tiktok$/i
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+    if (!text) throw `uhm.. urlnya mana?\n\npenggunaan:\n${usedPrefix + command} url\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/ZGJBtcsDq/`
+    if (!/https?:\/\/(www\.|v(t|m)\.|t\.)?tiktok\.com/i.test(text)) throw `url salah!`
+    let res = await fetch(API('amel', '/tiktok', { url: text }, 'apikey'))
+    if (!res.ok) throw eror
+    let json = await res.json()
+    if (!json.status) throw json
+    await m.reply(wait)
+    await conn.sendFile(m.chat, json.result.data.videoSD, 'tiktok.mp4', wm, m)
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['download']
+handler.command = /^(tiktok|tt)$/i
+
+handler.limit = 1
 
 module.exports = handler
