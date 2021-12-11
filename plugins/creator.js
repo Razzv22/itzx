2d4f9646fb64435e73a5de5091b0eaa4679029bc
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-11 19:36:57 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   this.sendContact(m.chat, kontak[0], 'Akmalz', m)
+  this.sendContact(m.chat, kontak[1], 'Bot BETA 24 Jam', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
