c2b94ad4ef2702f70f6a8f9ab1b223170749ce35
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-14 13:25:56 +0700

    Update mention.js

--- plugins/mention.js
+++ plugins/mention.js
@@ -1,5 +1,6 @@
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'Tidak ada teks'
+  let users = text.split(',').map(v => v.replace(/[^0-9]/g, ''))
   m.reply(text, false, {
     contextInfo: {
       mentionedJid: conn.parseMention(text)
