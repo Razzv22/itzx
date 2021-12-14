895624b2a912316f1c16959ed5880e964a7f9513
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-14 13:12:08 +0700

    Update open.js

--- plugins/open.js
+++ plugins/open.js
@@ -1,3 +1,4 @@
+/*
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, command, args, text, usedPrefix, DevMode }) => {
 	if (!db.data.chats[m.chat].rpg && m.isGroup) throw global.wait
@@ -643,3 +644,4 @@ module.exports = handler
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
+*/
